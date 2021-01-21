ccffef45ef1cbc937cab768cf0a4d9c235a62081
Zawar Nadeem <zawar.nadeem@gsquad.io>
2021-01-21 14:53:21 +0500

    KA-203 - created activities for onboarding - splash

---
+++ app/src/main/java/ai/kaira/app/Onboarding/OnboardActivity.kt
@@ -0,0 +1,11 @@
+package ai.kaira.app.Onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
--- app/src/main/java/ai/kaira/app/Splash/SplashActivity.kt
+++ app/src/main/java/ai/kaira/app/Splash/SplashActivity.kt
@@ -1,11 +1,11 @@
-package ai.kaira.app
+package ai.kaira.app.Splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
