44c4954b7bca74e9e19b3fa14526133e5766a355
Zawar Nadeem <zawar.nadeem@gsquad.io>
2021-05-06 12:19:57 +0500

    KA-481 - typo fix

--- app/src/main/java/ai/kaira/app/splash/SplashActivity.kt
+++ app/src/main/java/ai/kaira/app/splash/SplashActivity.kt
@@ -19,7 +19,6 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        )
         Timer().schedule(SPLASH_TIME_OUT){
             //do something
             if(isLoggedIn()){
