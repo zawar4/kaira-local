17aa9a19572c81422c48eebf8caeb1288e7c3f3e
Zawar Nadeem <zawar.nadeem@gsquad.io>
2021-06-10 13:59:17 +0500

    KA-580 - added banking accounting type

--- domain/src/main/java/ai/kaira/domain/financial/model/AssetItem.kt
+++ domain/src/main/java/ai/kaira/domain/financial/model/AssetItem.kt
@@ -1,7 +1,8 @@
 package ai.kaira.domain.financial.model
 
+import ai.kaira.domain.banking.institution.model.BankingAccountingType
 import ai.kaira.domain.banking.institution.model.Institution
 import androidx.annotation.Keep
 
 @Keep
-data class AssetItem(val id : String, val type : String, val balance : Double, val accountingType : Int, val title : String, val institution : Institution)
+data class AssetItem(val id : String, val type : String, val balance : Double, val accountingType : BankingAccountingType, val title : String, val institution : Institution)
--- domain/src/main/java/ai/kaira/domain/financial/model/LiabilityItem.kt
+++ domain/src/main/java/ai/kaira/domain/financial/model/LiabilityItem.kt
@@ -1,5 +1,6 @@
 package ai.kaira.domain.financial.model
 
+import ai.kaira.domain.banking.institution.model.BankingAccountingType
 import ai.kaira.domain.banking.institution.model.Institution
 
-data class LiabilityItem(val id : String, val type : String, val balance : Double, val accountingType : Int, val title : String, val institution : Institution)
+data class LiabilityItem(val id : String, val type : String, val balance : Double, val accountingType : BankingAccountingType, val title : String, val institution : Institution?)
